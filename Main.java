8705b9e294a7dcff1ee7a4a18223a53c2cfdf0c0
basti <bastian_behrendt@yahoo.de>
2024-03-08 12:01:17 +0100

    Step 1: Create an array with numbers from 1 to 10 and print it to the console.

--- Main.java
+++ Main.java
@@ -2,18 +2,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        arrayPrint();
+        //int[] printArrayBig = {11,12,13,14,15,16,17,18,19,20};
+        //arrayPrint();
+        arraySum();
 
     }
 
     public static void arrayPrint(){
         int[] printArray = {1,2,3,4,5,6,7,8,9,10};
-
         for (int i = 0; i < printArray.length; i++){
             System.out.println("Zählerposition: " + i + " Inhalt array: " + printArray[i]);
         }
-
     }
 
-
+    public static void arraySum(){
+        int[] printArray = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for (int j : printArray) {
+            sum = sum + j;
+            System.out.println(sum);
+        }
+    }
 }
